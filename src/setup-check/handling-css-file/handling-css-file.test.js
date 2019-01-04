16e36198f807230380081dcb715c1cb6e24c9e0e
Apollo Tang <tang.apollo@gmail.com>
2019-01-03 22:43:56 -0500

    fix bug

--- src/setup-check/handling-css-file/handling-css-file.test.js
+++ src/setup-check/handling-css-file/handling-css-file.test.js
@@ -1,7 +1,7 @@
 import configuration from '../jest-setup-check-configuration.js';
 const styleMock = require(`${configuration().testFolderPath}/style-mock.js`);
 
-import importedCssFilefrom from './handle-css-file.js';
+import importedCssFilefrom from './handling-css-file.js';
 
 test('It handle css file', ()=>{
   expect(importedCssFilefrom).toEqual(styleMock);
