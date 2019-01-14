ad0e232641e53757b3e337806317ad710e7628a6
Apollo Tang <tang.apollo@gmail.com>
2019-01-14 10:54:54 -0500

    clean up jest.config.js, commented, added testPathIgnorePatterns and projects entry

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,32 @@
 const {defaults} = require('jest-config');
+
 module.exports = {
+
+  // "testMatch": [ "**/__jest__/**/*.js?(x)", "**/?(*.)+(jest).js?(x)" ], // <--- if use this jest won't compile typescript
+
+  "testPathIgnorePatterns": [
+    "/nodule_module/",
+  ],
+
+  "projects" : [
+    "src"
+  ],
+
   // "testEnvironment": 'node',
   "testEnvironment": 'jsdom',
+
   "setupFiles": [
     // collection of script to run before jest is loaded
     "<rootDir>/test/jest.init.js"
   ],
+
   setupFilesAfterEnv: [
     require.resolve('./test/setup-tests.js'), // path to script after jest is loaded
   ],
+
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js')
   }
-}
+
+};
