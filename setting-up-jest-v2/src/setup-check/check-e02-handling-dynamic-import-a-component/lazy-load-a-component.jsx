0d916a0df4f887f9a081b46ec96b7bd166a37bf6
Apollo Tang <tang.apollo@gmail.com>
2019-08-16 15:43:11 -0400

    add comment: for core-js polyfill required for react-loadable

--- setting-up-jest-v2/src/setup-check/check-e02-handling-dynamic-import-a-component/lazy-load-a-component.jsx
+++ setting-up-jest-v2/src/setup-check/check-e02-handling-dynamic-import-a-component/lazy-load-a-component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Loadable from 'react-loadable';
+import Loadable from 'react-loadable'
 import 'core-js/fn/object/assign'
+// core-js/fn/object/assing is added above  b/c
+// react-loadable did not polyfill Object.assign
 
 const promise = new Promise((rs) => {
   const p = import('./component-to-lazy-load.jsx');
